e46065db2720cab52ee71e4726c8c2ab3cea3b35
Andreyzc02 <andreyztcl@gmail.com>
2025-10-10 15:43:48 -0300

    Audio page update 2

--- flutter_application_1/lib/pages/AudioPage.dart
+++ flutter_application_1/lib/pages/AudioPage.dart
@@ -21,47 +21,6 @@ class _AudioPageState extends State<AudioPage> {
   bool carregandoNatureza = false;
   bool carregandoMusicas = false;
 
-<<<<<<< HEAD
-=======
-  List<Map<String, String>> naturezaList = [
-    {
-      'text': 'Chuva',
-      'url': 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
-    },
-    {
-      'text': 'Pássaros cantando',
-      'url': 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
-    },
-    {
-      'text': 'Murmúrio do riacho',
-      'url': 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3',
-    },
-    {
-      'text': 'Vento suave', // Novo card natureza
-      'url': 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3',
-    },
-  ];
-
-  List<Map<String, String>> musicasList = [
-    {
-      'text': 'Trilha relaxante',
-      'url': 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3',
-    },
-    {
-      'text': 'Música calma',
-      'url': 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3',
-    },
-    {
-      'text': 'Som instrumental',
-      'url': 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3',
-    },
-    {
-      'text': 'Piano suave', // Novo card musica
-      'url': 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3',
-    },
-  ];
-
->>>>>>> 23cbd871f728d80b18f8f688b2dcfa429496ce4d
   @override
   void dispose() {
     _audioPlayer.dispose();
